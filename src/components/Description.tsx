c9da2bcb5cc3876984aa2db4e1e810b2838d09ad
Ten Suzuki <stuzeukni0901@icloud.com>
2024-10-29 17:22:33 +0900

    revise ui

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -37,7 +37,7 @@ const Description: React.FC<DescriptionProps> = ({ setState,nickname,setNickname
         setState('interpretation');
     };
 return (
-    <Tabs defaultValue="account" className="w-[400px]">
+    <Tabs defaultValue="account" className="w-fit max-w-screen-md">
     <TabsList className="grid w-full grid-cols-2">
         <TabsTrigger value="account">TATについて</TabsTrigger>
         <TabsTrigger value="password">始める</TabsTrigger>
@@ -45,14 +45,14 @@ return (
     <TabsContent value="account">
         <Card>
         <CardHeader>
-            <CardTitle>概要</CardTitle>
+            <CardTitle className="text-start">概要</CardTitle>
             <CardDescription className="text-start">
             TAT（Thematic Apperception Test）とは、1943年にマレーとモーガンを中心としたハーバード心理学クリニックのスタッフが作成した性格検査です。別名「主題統覚検査」とも呼ばれます。<br />
             ある場面が描かれた20枚のカードを提示され、それぞれのカードに対して想像して物語を作る検査方法です
             </CardDescription>
         </CardHeader>
         <CardHeader>
-            <CardTitle>流れ</CardTitle>
+            <CardTitle className="text-start">流れ</CardTitle>
             <CardDescription className="text-start">
             これから色々な人や景色の書かれた絵が表示されます。この絵を見て思い浮かぶ物語を作って、入力してください。<br />
             この絵の中の人は今何を感じ、どうしているのか、この絵の前にはどんなことがあって、この絵の後にはどうなっていくのか、話の筋を付けて話してください。<br />
@@ -61,8 +61,8 @@ return (
             </CardDescription>
         </CardHeader>
         <CardHeader>
-            <CardTitle>注意点</CardTitle>
-            <CardDescription>
+            <CardTitle className="text-start">注意点</CardTitle>
+            <CardDescription className="text-start">
             特になし
             </CardDescription>
         </CardHeader>
